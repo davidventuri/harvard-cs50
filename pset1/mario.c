40a9618624cb9e113b63947e72d2758993cfa23c
David Venturi <venturidb@gmail.com>
2015-11-17 12:18:07 -0500

    Update mario.c
    Fixed style

--- pset1/mario.c
+++ pset1/mario.c
@@ -21,12 +21,12 @@ int main(void)
     while (height < 1 || height > 23); // requests an integer from the user until the above request is satisfied 
     printf("Thank you for the integer!\nHere is Mario's half-pyramid of height %i:\n", height);
     
-    width = height + 1; // + 1 is necessary because the top row needs two hashes 
-    for(row_counter = 1; row_counter <= height; row_counter++)
+    width = height + 1; // +1 is necessary because the top row needs two hashes 
+    for (row_counter = 1; row_counter <= height; row_counter++)  
     {
-        for(col_counter = 1; col_counter <= width; col_counter++)
+        for (col_counter = 1; col_counter <= width; col_counter++)
         {
-            if(col_counter <= width - (row_counter + 1)) // the nth row has width-(n+1) spaces... 
+            if (col_counter <= width - (row_counter + 1)) // the nth row has width-(n+1) spaces... 
                 printf(" ");
             else
                 printf("#"); // ...and (n+1) hashes
