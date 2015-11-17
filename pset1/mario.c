6a56d0e5c8054f63999a92ee72b6d6796ba76f99
David Venturi <venturidb@gmail.com>
2015-11-17 11:47:11 -0500

    Create mario.c

---
+++ pset1/mario.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int height, width, row_counter, col_counter;
+    
+    do
+    {
+        printf("Please give me an integer between 1 and 23: "); 
+        height = GetInt();  
+    }
+    while (height < 1 || height > 23); // requests an integer from the user until the above request is satisfied 
+    printf("Thank you for the integer!\nHere is Mario's half-pyramid of height %i:\n", height);
+    
+    width = height + 1; // + 1 is necessary because the top row needs two hashes 
+    for(row_counter = 1; row_counter <= height; row_counter++)
+    {
+        for(col_counter = 1; col_counter <= width; col_counter++)
+        {
+            if(col_counter <= width - (row_counter + 1)) // the nth row has width-(n+1) spaces... 
+                printf(" ");
+            else
+                printf("#"); // ...and (n+1) hashes
+        }  
+        printf("\n");
+    } 
+}
