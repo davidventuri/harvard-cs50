34fdc52ca1b1e558680ed1cbbbeceb751ce52369
David Venturi <venturidb@gmail.com>
2016-02-08 23:47:05 -0500

    Create dictionary.c

---
+++ pset5/dictionary.c
@@ -0,0 +1,48 @@
+/**
+ * dictionary.c
+ *
+ * Computer Science 50
+ * Problem Set 5
+ *
+ * Implements a dictionary's functionality.
+ */
+
+#include <stdbool.h>
+
+#include "dictionary.h"
+
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check(const char* word)
+{
+    // TODO
+    return false;
+}
+
+/**
+ * Loads dictionary into memory.  Returns true if successful else false.
+ */
+bool load(const char* dictionary)
+{
+    // TODO
+    return false;
+}
+
+/**
+ * Returns number of words in dictionary if loaded else 0 if not yet loaded.
+ */
+unsigned int size(void)
+{
+    // TODO
+    return 0;
+}
+
+/**
+ * Unloads dictionary from memory.  Returns true if successful else false.
+ */
+bool unload(void)
+{
+    // TODO
+    return false;
+}
