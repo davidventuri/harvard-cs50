98bbfb49b879b47750d41f3e4a219c6536eb675e
David Venturi <venturidb@gmail.com>
2016-02-12 02:20:36 -0500

    Update dictionary.c (work in progress)

--- pset5/dictionary.c
+++ pset5/dictionary.c
@@ -12,7 +12,9 @@
 #include "dictionary.h"
 
 /**
- * Returns true if word is in dictionary else false.
+ * Returns true if word is in dictionary else false. Case-insensitive.
+ * Assume that check is only passed strings with alphabetical characters and/or
+ * apostrophes.
  */
 bool check(const char* word)
 {
@@ -21,11 +23,45 @@ bool check(const char* word)
 }
 
 /**
- * Loads dictionary into memory.  Returns true if successful else false.
+ * Loads dictionary into memory. Stores words in hash table. Returns true if
+ * successful else false.
  */
 bool load(const char* dictionary)
 {
-    // TODO
+    // define struct for value-pointer pair, i.e., a node
+    typedef struct node
+    {
+        char word[LENGTH + 1];
+        struct node* next;
+    }
+    node;
+    
+    // open dictionary
+    FILE* fp = fopen(dictionary, "r");
+    if (fp == NULL)
+    {
+        printf("Could not open dictionary.\n");
+        return false;
+    }
+    
+    // read word from dictionary
+    // malloc a node* for each new word
+    while (true)
+    {
+        node* new_node = malloc(sizeof(node));
+        fscanf(fp, "%s", new_node->word);
+        free(new_node);
+        
+        if (feof(fp))
+        {
+            // hit end of file
+            break;
+        }
+    }
+    
+    // close dictionary
+    fclose(dict);
+    
     return false;
 }
 
