bfd07a59aeee6c97bbd7ad86a1f92b093457bda7
David Venturi <venturidb@gmail.com>
2016-02-26 11:03:58 -0500

    Create register.php

---
+++ pset7/public/register.php
@@ -0,0 +1,19 @@
+<?php
+
+    // configuration
+    require("../includes/config.php");
+
+    // if user reached page via GET (as by clicking a link or via redirect)
+    if ($_SERVER["REQUEST_METHOD"] == "GET")
+    {
+        // else render form
+        render("register_form.php", ["title" => "Register"]);
+    }
+
+    // else if user reached page via POST (as by submitting a form via POST)
+    else if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        // TODO
+    }
+
+?>
