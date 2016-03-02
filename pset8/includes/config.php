ac2be2b29f2e166502fcd7deb1ce29a850ca57f6
David Venturi <venturidb@gmail.com>
2016-03-01 23:16:47 -0500

    Create config.php

---
+++ pset8/includes/config.php
@@ -0,0 +1,23 @@
+<?php
+
+    /**
+     * config.php
+     *
+     * Computer Science 50
+     * Problem Set 8
+     *
+     * Configures pages.
+     */
+
+    // display errors, warnings, and notices
+    ini_set("display_errors", true);
+    error_reporting(E_ALL);
+
+    // requirements
+    require("helpers.php");
+
+    // CS50 Library
+    require("../vendor/library50-php-5/CS50/CS50.php");
+    CS50::init(__DIR__ . "/../config.json");
+
+?>
