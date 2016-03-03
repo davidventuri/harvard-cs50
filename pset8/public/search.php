14e6ddf0a9f59aef02417315949f5945251de4eb
David Venturi <venturidb@gmail.com>
2016-03-03 13:49:11 -0500

    Create search.php

---
+++ pset8/public/search.php
@@ -0,0 +1,20 @@
+<?php
+
+    require(__DIR__ . "/../includes/config.php");
+
+    // numerically indexed array of places
+    $places = [];
+
+    // fulltext search for geo field's value on joint key in database
+    $rows = CS50::query("SELECT * FROM places WHERE MATCH(postal_code, place_name, admin_name1, admin_name2) AGAINST (?)", $_GET["geo"]);
+    
+    foreach ($rows as $row)
+    {
+        array_push($places, $row);
+    }
+
+    // output places as JSON (pretty-printed for debugging convenience)
+    header("Content-type: application/json");
+    print(json_encode($places, JSON_PRETTY_PRINT));
+
+?>
